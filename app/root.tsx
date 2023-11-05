447281a1bd5bf95d65fb0c78630edeb4b1a287a4
gouravrajbit <gouravr244@gmail.com>
2023-11-05 15:37:29 +0530

    Load contact data

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { json } from "@remix-run/node";
 import {
   Form,
   Link,
@@ -7,9 +8,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
 
 import type { LinksFunction } from "@remix-run/node";
+import { getContacts } from "./data";
 // existing imports
 
 import appStylesHref from "./app.css";
@@ -18,8 +21,15 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
 
 export default function App() {
+  const { contacts } = useLoaderData();
+
   return (
     <html lang="en">
       <head>
@@ -55,6 +65,30 @@ export default function App() {
                 <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
         <div id="detail">
